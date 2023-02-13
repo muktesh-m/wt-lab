175ea042a86cf8960798ed666976cc8de6d6d5c8
Muktesh Mishra <110118880+muktesh-m@users.noreply.github.com>
2023-02-13 13:19:30 +0530

    Add files via upload

---
+++ lab 6/q1_evenodd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class q1_evenodd{
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }       
+
+    }
+}
